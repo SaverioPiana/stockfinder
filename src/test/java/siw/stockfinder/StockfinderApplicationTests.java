2edf467c3e6944e041883fa870707f112b56e21e
SaverioPiana <pianasave@gmail.com>
2023-05-25 15:10:01 +0200

    Initial commit

---
+++ src/test/java/siw/stockfinder/StockfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package siw.stockfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
