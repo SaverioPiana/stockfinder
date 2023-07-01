72765087f4ad953a567981f61282e45e0f1a24b7
Forest904 <lucapalocco@gmail.com>
2023-07-01 19:22:07 +0200

    Trovato problema nella validazione utenti e risolto, aggiunto avviso di errore se username gia esistente

--- src/main/java/siw/stockfinder/controller/AuthenticationController.java
+++ src/main/java/siw/stockfinder/controller/AuthenticationController.java
@@ -91,6 +91,6 @@ public class AuthenticationController {
             model.addAttribute("user", user);
             return "registrationSuccessful";
         }
-        return "registerUser";
+        return "formRegisterUser";
     }
 }
